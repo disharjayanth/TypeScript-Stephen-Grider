a54c6a5130f31bdb29db9a2af2137177411b6353
disharjayantha <dishuj15@gmail.com>
2020-12-23 02:05:48 +0530

    map app complete

--- 02_maps/src/index.ts
+++ 02_maps/src/index.ts
@@ -1,13 +1,10 @@
 import { User } from "./User"
 import { Company } from "./Company"
-import 'googlemaps'
+import { CustomMap } from "./CustomMap"
 
 const user = new User()
-
-console.log(user)
-
 const company = new Company()
+const customMap = new CustomMap("map")
 
-console.log(company)
-
-google
+customMap.addMarker(user)
+customMap.addMarker(company)
