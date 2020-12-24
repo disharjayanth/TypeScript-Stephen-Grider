bb2a65a36a950bd9a5b0f39c2ef6f09ff9da3c37
disharjayantha <dishuj15@gmail.com>
2020-12-24 06:32:06 +0530

    sort complete

--- 03_sort/build/CharactersCollection.js
+++ 03_sort/build/CharactersCollection.js
@@ -1,9 +1,26 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CharacterCollections = void 0;
-var CharacterCollections = /** @class */ (function () {
+var Sorter_1 = require("./Sorter");
+var CharacterCollections = /** @class */ (function (_super) {
+    __extends(CharacterCollections, _super);
     function CharacterCollections(data) {
-        this.data = data;
+        var _this = _super.call(this) || this;
+        _this.data = data;
+        return _this;
     }
     Object.defineProperty(CharacterCollections.prototype, "length", {
         get: function () {
@@ -23,5 +40,5 @@ var CharacterCollections = /** @class */ (function () {
         this.data = characters.join("");
     };
     return CharacterCollections;
-}());
+}(Sorter_1.Sorter));
 exports.CharacterCollections = CharacterCollections;
